e14947db5da4915222f9efaadb38ff6fece1b929
spar3chang3 <baker.daryk@ic.edu>
2025-02-22 05:38:35 -0600

    Initial commit with most of alarm section done

---
+++ src/lib/Alarm.svelte
@@ -0,0 +1,12 @@
+<script lang="js">
+
+    let alarmTime = $props();
+
+</script>
+<style lang="css">
+
+</style>
+<div class="alarm-comp">
+    <span>{alarmTime}</span>
+    <button class="delete">&#10005;</button>
+</div>
