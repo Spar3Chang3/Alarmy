5c8b5952ee2cc37b7b1372a0824a050494b461a1
spar3chang3 <baker.daryk@ic.edu>
2025-02-28 17:37:29 -0600

    Mostly finish Timer component with lots of problems

--- src/lib/Timer.svelte
+++ src/lib/Timer.svelte
@@ -1,17 +1,72 @@
 <script lang="js">
-	import { IconLinks } from '$lib/index.js';
+	import { FormatNumberString, IconLinks } from '$lib/index.js';
+	import { onMount } from 'svelte';
 
-	let { totalTime = 0, hours = 0, minutes = 0, seconds = 0, isRinging = $bindable(false) } = $props();
+	let { totalTimeMs = 0, isRinging = $bindable(false), callback = (timer) => {} } = $props();
 
+	let animationFrame = $state();
+
+	let startTime = $state(0);
 	let elapsedTime = $state(0);
 	let prevElapsedTime = $state(0);
 
 	let timeLeft = $state(0);
-	let displayTimeLeft = $state('00:00:00');
+	let displayTimeLeft = $derived(`${FormatNumberString(hours)}:${FormatNumberString(minutes)}:${FormatNumberString(seconds)}`);
+
+	let hours = $state(0);
+	let minutes = $state(0);
+	let seconds = $state(0);
 
 	let isRunning = $state(false);
 
 	let displayTotalTime = $state('00:00:00');
+
+	function startTimer(e) {
+		if (e) {
+			e.preventDefault();
+		}
+
+		startTime = performance.now();
+		isRunning = true;
+		animationFrame = requestAnimationFrame(countDown);
+	}
+
+	function stopTimer(e) {
+		if (e) {
+			e.preventDefault();
+		}
+
+		prevElapsedTime = performance.now() - startTime;
+		isRunning = false;
+		cancelAnimationFrame(animationFrame);
+	}
+
+	function deleteTimer(e) {
+		if (e) {
+			e.preventDefault();
+		}
+
+		callback(totalTimeMs);
+	}
+
+	function countDown() {
+		elapsedTime = (performance.now() - startTime) + prevElapsedTime;
+		timeLeft = totalTimeMs - elapsedTime;
+
+		hours = Math.floor(timeLeft / 3600000);
+		minutes = Math.floor(timeLeft / 60000);
+		seconds = Math.floor(timeLeft / 1000);
+	}
+
+	onMount(() => {
+		console.log(totalTimeMs);
+
+		const hours = Math.floor(totalTimeMs / 3600000);
+		const minutes = Math.floor(totalTimeMs / 60000);
+		const seconds = Math.floor(totalTimeMs / 1000);
+
+		displayTotalTime = `${FormatNumberString(hours)}:${FormatNumberString(minutes)}:${FormatNumberString(seconds)}`;
+	});
 
 </script>
 <style>
@@ -22,7 +77,7 @@
 				grid-template-columns: 34% 33% 33%;
 
 				height: fit-content;
-				max-width: 12rem;
+				max-width: 16rem;
 
 				place-items: center;
 				gap: 0.5rem;
@@ -35,6 +90,8 @@
 				height: 90%;
 				width: fit-content;
 
+				place-items: center;
+
 				border-radius: 50%;
 				z-index: 7;
 		}
@@ -42,12 +99,63 @@
 		.circle {
 				position: absolute;
 
-				height: 110%;
-				width: 110%;
+				height: 100%;
+				width: 100%;
 
 				animation: fillCircle;
 
 				z-index: 6;
+		}
+
+		.time-total {
+				display: grid;
+
+				height: 100%;
+				width: 100%;
+
+				place-items: center;
+		}
+
+		.controls {
+				position: relative;
+				display: grid;
+				grid-template-rows: 50% 50%;
+
+				height: 100%;
+				width: 100%;
+
+				place-items: center;
+				gap: 0.5rem;
+		}
+
+		.start-stop-btn, .delete-btn{
+				height: 3rem;
+				width: 3rem;
+
+				border: none;
+				border-radius: 12px;
+				cursor: pointer;
+				padding: 0.5rem;
+				margin: 0 auto;
+		}
+
+		.start-stop-btn {
+				background-color: var(--color-accent);
+		}
+
+		.delete-btn {
+				background-color: var(--color-foreground);
+		}
+
+		.controls img {
+				position: relative;
+				height: 2.5rem;
+				width: 2.5rem;
+
+				object-fit: contain;
+				object-position: center;
+				margin: 0 auto;
+				padding: 0;
 		}
 
     @keyframes fillCircle {
@@ -61,14 +169,14 @@
 </style>
 <div class="timer-comp">
 	<div class="time-circle">
-		<div class="circle" style="animation-duration: {totalTime}ms; animation-play-state: {isRunning ? 'playing' : 'paused'}"></div>
+		<div class="circle" style="animation-duration: {totalTimeMs}ms; animation-play-state: {isRunning ? 'playing' : 'paused'}"></div>
 		<span>{displayTimeLeft}</span>
 	</div>
 	<div class="time-total">
 		<span>{displayTotalTime} timer</span>
 	</div>
 	<div class="controls">
-		<button class="start-stop-btn" aria-label="Start/Stop"><img src={isRunning ? IconLinks.stop : IconLinks.play} alt="play/pause icon"/></button>
-		<button class="delete-btn" aria-label="Delete"><img src={IconLinks.trash} alt="delete"/></button>
+		<button class="start-stop-btn" aria-label="Start/Stop" onclick={isRunning ? stopTimer : startTimer}><img src={isRunning ? IconLinks.stop : IconLinks.play} alt="play/pause icon"/></button>
+		<button class="delete-btn" aria-label="Delete" onclick={deleteTimer}><img src={IconLinks.trash} alt="delete"/></button>
 	</div>
 </div>
--- src/routes/timer/+page.svelte
+++ src/routes/timer/+page.svelte
@@ -6,6 +6,7 @@
     import CookiePrompt from '$lib/CookiePrompt.svelte';
     import Fireflies from '$lib/Fireflies.svelte';
     import TimePicker from '$lib/TimePicker.svelte';
+    import Timer from '$lib/Timer.svelte';
 
     const cookieName = "TimerTimes";
 
@@ -14,20 +15,55 @@
 
     let timerSet = $state(false);
 
-    let hours = $state(0);
-    let minutes = $state(0);
-    let seconds = $state(0);
+    let savedTimers = $state(new Map());
+    let displaySavedTimers = $state([]);
+
+    function deleteTimer(timerToDelete) {
+        savedTimers.delete(timerToDelete.totalTimeMs);
+    }
+
+    onMount(() => {
+        savedTimers.set((60 * 60000), {
+           totalTimerMs: (60 * 60000),
+        });
+
+        displaySavedTimers = [...savedTimers.values()];
+    });
 
 </script>
 <style lang="css">
 
+    .timer {
+        position: relative;
+        display: grid;
 
+        min-height: 100dvh;
+        width: 100dvw;
+        max-width: 100dvw;
+
+        place-items: center;
+    }
+
+    .timer-comp-container {
+        position: relative;
+        display: flex;
+        flex-wrap: wrap;
+
+        height: fit-content;
+        max-width: 90dvw;
+
+        justify-content: space-around;
+
+        z-index: 5;
+    }
 
 </style>
 <section class="timer">
     <div class="shelf"></div>
     <div class="timer-comp-container">
-
+        {#each displaySavedTimers as timer}
+            <Timer totalTimeMs={60*1000} callback={deleteTimer}/>
+        {/each}
     </div>
     <div class="new-timer-container">
         <TimePicker/>
